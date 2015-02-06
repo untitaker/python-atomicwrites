a991ad360d18d399aa23779b8779a83c23d4a60a
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-02-06 16:33:20 +0100

    Add testcase

--- tests/test_atomicwrites.py
+++ tests/test_atomicwrites.py
@@ -23,3 +23,12 @@ def test_teardown(tmpdir):
             assert False
 
     assert not tmpdir.listdir()
+
+
+def test_atomic_write_replace_simultaneously_created_file(tmpdir):
+    fname = tmpdir.join('ha')
+    with atomic_write(str(fname), overwrite=True) as f:
+        f.write('hoho')
+        fname.write('harhar')
+        assert fname.read() == 'harhar'
+    assert fname.read() == 'hoho'
