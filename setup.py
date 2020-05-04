e8bb206e0e9283bded452616a18e81ffeedce698
Jürgen Gmach <juergen.gmach@googlemail.com>
2020-05-04 11:34:04 +0200

    Add support for Python 3.8
    Also...
    - remove obsolete `sudo` keyword from Travis config
    (https://blog.travis-ci.com/2018-11-19-required-linux-infrastructure-migration)
    - additionally use 64 bit interpreters on AppVeyor
    
    modified:   .travis.yml
    modified:   appveyor.yml
    modified:   setup.py
    modified:   tox.ini

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
